496178cb986922dd7bde194142a6333d08afed75
ShashiSubramanya <shashikala.subramanya@thoughtspot.com>
2021-08-17 17:58:00 +0530

    updateddoc pages

--- docs/src/components/Header/index.tsx
+++ docs/src/components/Header/index.tsx
@@ -1,26 +1,55 @@
 import React from 'react';
 import TSLogo from '../../assets/svg/ts-logo-white.svg';
 import t from '../../utils/lang-utils';
+// import Dropdown from '../Dropdown';
 import './index.scss';
 
-const Header = () => (
-    <header>
-        <section className="container">
-            <div className="headerWrapper">
-                <div>
-                    <h1 className="m-0">
-                        <a href="#" title={t('TS_LOGO_ALT_TEXT')}>
-                            <img
-                                src={TSLogo}
-                                alt={t('TS_LOGO_ALT_TEXT')}
-                                className="thoughtspotLogo"
-                            />
+const Header = (props: { location: Location }) => {
+    const communityLinks = [
+        { name: 'Playground', href: 'https://try-everywhere.thoughtspot.cloud/v2/#/everywhere/playground/search' },
+        {
+            name: 'Community',
+            href:
+                'https://community.thoughtspot.com/customers/s/topic/0TO3n000000erVyGAI/developers',
+        },
+        { name: 'Support', href: 'https://www.thoughtspot.com/support' },
+        { name: 'Help Center', href: 'https://cloud-docs.thoughtspot.com/' },
+    ];
+
+    return (
+        <header>
+            <section className="containerWrapper">
+                <div className="headerWrapper">
+                    <div>
+                        <h2 className="m-0 d-inline-block logo">
+                            <a href="#" title={t('TS_LOGO_ALT_TEXT')}>
+                                <img
+                                    src={TSLogo}
+                                    alt={t('TS_LOGO_ALT_TEXT')}
+                                    className="thoughtspotLogo"
+                                />
+                            </a>
+                        </h2>
+                        <a
+                            className="m-0 d-inline-block docsWrapper"
+                            href="?pageid=introduction"
+                        >
+                            {t('DOCS')}
                         </a>
-                    </h1>
+                        <div className="d-inline-block headerLink">
+                            {communityLinks.map(({ name, href }) => (
+                                <a href={href} target="_blank">
+                                    {name}
+                                </a>
+                            ))}
+
+                        </div>
+                    </div>
+                    {/*  <Dropdown location={props.location} /> */}
                 </div>
-            </div>
-        </section>
-    </header>
-);
+            </section>
+        </header>
+    );
+};
 
 export default Header;
--- docs/src/configs/doc-configs.js
+++ docs/src/configs/doc-configs.js
@@ -1,5 +1,5 @@
 module.exports = {
-    DOC_REPO_NAME: '/visual-embed-sdk',
+    DOC_REPO_NAME: '',
     DOC_NAV_PAGE_ID: 'nav',
     TS_HOST_PARAM: 'tshost',
     TS_ORIGIN_PARAM: 'origin',
@@ -9,6 +9,7 @@ module.exports = {
     PREVIEW_PREFIX: 'previewPrefix',
     VISUAL_EMBED_SDK_PREFIX: 'visualEmbedSDKPrefix',
     NOT_FOUND_PAGE_ID: '404-error',
+    HOME_PAGE_ID: 'introduction',
     NOT_FOUND_GO_HOME_PAGE_ID: 'introduction',
     TYPE_DOC_PREFIX: 'typedoc',
     DEFAULT_HOST: 'https://try-everywhere.thoughtspot.cloud',
@@ -19,10 +20,25 @@ module.exports = {
         PROD: 'PROD',
         DEV: 'DEV',
         STAGING: 'STAGING',
+        PROD_VERSIONING: 'PROD_VERSIONING',
         LOCAL: 'LOCAL',
     },
     DEPLOY_ENVS: {
         RELEASE: 'release',
         DEV: 'dev',
     },
+    VERSION_DROPDOWN: [
+        {
+            label: 'Cloud',
+            link: '/visual-embed-sdk/release/en',
+        },
+        {
+            label: 'Dev',
+            link: '/visual-embed-sdk/dev/en',
+        },
+        {
+            label: '7.1',
+            link: '/visual-embed-sdk/7.1/en',
+        },
+    ],
 };
