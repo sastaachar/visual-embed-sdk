70b6e2ad79f833e556275f6094702cbff0dc8d7d
kailash-choudhary-thoughtspot <kailash.choudhary@thoughtspot.com>
2021-10-02 13:53:18 +0530

    debug node14 error

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,9 +2,5 @@ const fsExtra = require('fs-extra');
 
 exports.onPostBuild = () => {
 
-  fsExtra.copyFileSync(`${__dirname}/robots.txt`, `${__dirname}/public/robots.txt`, err => {
-    if (err) {
-      console.error('Error copying file', err);
-    }
-  });
+    fsExtra.copyFileSync(`${__dirname}/robots.txt`, `${__dirname}/public/robots.txt`);
 };
