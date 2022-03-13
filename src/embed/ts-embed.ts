d2a53222fd21e92307ce6ffce1fc83240c222317
Akash Mohapatra <akash.mohapatra@thoughtspot.com>
2022-03-14 01:16:18 +0530

    [SCAL-111875] disableLoginRedirect implementation

--- src/embed/ts-embed.ts
+++ src/embed/ts-embed.ts
@@ -324,7 +324,9 @@ export class TsEmbed {
         queryParams[Param.ViewPortHeight] = window.innerHeight;
         queryParams[Param.ViewPortWidth] = window.innerWidth;
         queryParams[Param.Version] = version;
-
+        if (this.embedConfig.disableLoginRedirect === true) {
+            queryParams[Param.DisableLoginRedirect] = this.embedConfig.disableLoginRedirect;
+        }
         if (this.embedConfig.customCssUrl) {
             queryParams[Param.CustomCSSUrl] = this.embedConfig.customCssUrl;
         }
